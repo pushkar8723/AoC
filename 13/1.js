b27718f25b486582c5bc795ec361374fb474118a
Pushkar Anand <pushkar8723@gmail.com>
2019-12-13 23:26:12 +0530

    Day 13 completed.

--- 13/1.js
+++ 13/1.js
@@ -1,49 +1,69 @@
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
+let board = {};
+let grid = [];
+let paddlePosition;
+let ballPosition;
 const intcode = require('../shared/intcode').default;
 
+const printGrid = async () => {
+    process.stdout.write('\033c');
+    const tiles =[' ', '#', 'X', '_', 'O'];
+    for (let j = 0; j < 24; j++) {
+        for (let i = 0; i < 42; i++) {
+            process.stdout.write(tiles[board[[i, j].toString()]]);
+        }
+        process.stdout.write('\n');
+    }
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, 50);
+    });
+}
 
-
-const playgame = (machine, nextInput, internals, initScore) => {
-    machine.setInternals(internals);
-    let output = machine.exec([ nextInput ]);
+const playgame = async (machine) => {
+    let output = machine.exec();
     let score = 0;
     while (true) {
         if (!output.done) {
             if (output.inputRequired) {
-                const internals = machine.getInternals();
-                score = Math.max(
-                    playgame(machine, 0, internals, Math.max(initScore, score)),
-                    // playgame(machine, 1, internals, Math.max(initScore, score)),
-                    // playgame(machine, -1, internals, Math.max(initScore, score))
-                );
+                await printGrid();
+                let input = paddlePosition - ballPosition;
+                if (input > 0) {
+                    input = -1;
+                } else if (input < 0) {
+                    input = 1;
+                }
+                output = machine.exec([input]);
+            }
+            const x = output.value, y = machine.exec().value, z = machine.exec().value;
+            // console.log(x, y, z);
+            if (x === -1) {
+                score = z;
+                // console.log('Score:', score);
             } else {
-                const x = output.value, y = machine.exec().value, z = machine.exec().value;
-                if (x === -1) {
-                    score = z;
-                    if (score < initScore) {
-                        return -1;
-                    }
-                    console.log('MID:', score);
-                } else {
-                    console.log(x, y, z);
+                board[[x, y].toString()] = z;
+                if (z === 3) {
+                    paddlePosition = x;
+                } else if (z === 4) {
+                    ballPosition = x;
                 }
             }
             output = machine.exec();
         } else {
-            console.log('END:', score);
-            return score;
+            console.log('Score:', score);
+            break;
         }
     }
 }
 
-process.stdin.on('data', function (chunk) {
+process.stdin.on('data', async function (chunk) {
     const input = chunk.toString().split('\n')[0].split(',').map(item => Number(item));
     // Write your code here
     const machine = new intcode(input.slice(0));
     let output;
-    let grid = [];
     while (true) {
         output = machine.exec();
         if (!output.done) {
@@ -61,10 +81,5 @@ process.stdin.on('data', function (chunk) {
     const inp = input.slice(0);
     inp[0] = 2;
     machine.reset(inp);
-    const internals = machine.getInternals();
-    console.log(Math.max(
-        playgame(machine, 0, internals, 0),
-        // playgame(machine, 1, internals, 0),
-        // playgame(machine, -1, internals, 0)
-    ));
+    await playgame(machine);
 });
