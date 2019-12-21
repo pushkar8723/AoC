9ef94223952a403fc5a7425752aab0f404422ce7
Pushkar Anand <pushkar8723@gmail.com>
2019-12-21 11:05:07 +0530

    Day 21 part 1

---
+++ 21/1.js
@@ -0,0 +1,33 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+const intcode = require('../shared/intcode').default;
+const { asciiConv } = require('../shared/ascii');
+
+process.stdin.on('data', function (chunk) {
+    const input = chunk.toString().split('\n')[0].split(',').map(item => Number(item));
+    // Write your code here
+    const machine = new intcode(input.slice(0));
+    let output = machine.exec();
+    const sprintScript = asciiConv(
+        `NOT A T
+        NOT B J
+        OR T J
+        NOT C T
+        OR T J
+        AND D J
+        WALK
+        `);
+    while (!output.done) {
+        if (output.inputRequired) {
+            output = machine.exec(sprintScript.splice(0, 1));
+        } else {
+            if (output.value >= 0 && output.value < 256) {
+                process.stdout.write(String.fromCharCode(output.value));
+            } else {
+                console.log(output.value);
+            }
+            output = machine.exec();
+        }
+    }
+});
---
+++ shared/ascii.js
@@ -0,0 +1,5 @@
+const asciiConv = (str) => str.split('').map((item) => item.charCodeAt(0));
+
+module.exports = {
+    asciiConv
+};
