c144973f506ce0a7515f26dd3d6eb2fe22deb53d
Pushkar Anand <pushkar8723@gmail.com>
2019-12-22 10:53:48 +0530

    Day 21 part 2.

--- 21/1.js
+++ 21/1.js
@@ -30,4 +30,29 @@ process.stdin.on('data', function (chunk) {
             output = machine.exec();
         }
     }
+    const sprintScript2 = asciiConv(
+        `OR A J
+        AND B J
+        AND C J
+        NOT J J
+        AND D J
+        OR E T
+        OR H T
+        AND T J
+        RUN
+        `);
+    machine.reset(input.slice(0));
+    output = machine.exec();
+    while (!output.done) {
+        if (output.inputRequired) {
+            output = machine.exec(sprintScript2.splice(0, 1));
+        } else {
+            if (output.value >= 0 && output.value < 256) {
+                process.stdout.write(String.fromCharCode(output.value));
+            } else {
+                console.log(output.value);
+            }
+            output = machine.exec();
+        }
+    }
 });
