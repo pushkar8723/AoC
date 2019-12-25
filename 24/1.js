fad2b0c1fc150187b955a1a63e03309a698bcaee
Pushkar Anand <pushkar8723@gmail.com>
2019-12-25 11:16:30 +0530

    Day 24 completed.

--- 24/1.js
+++ 24/1.js
@@ -13,6 +13,8 @@ const XYtoI = (x, y) => {
     return -1;
 }
 
+const emptyLevel = '............?............';
+
 const getNext = (current) => {
     const next = [];
     for (let i = 0; i < current.length; i++) {
@@ -40,6 +42,139 @@ const getNext = (current) => {
     return next.join('');
 }
 
+const getNeighbours = (grid, level, i) => {
+    const current = grid[level];
+    const p = iToXY(i);
+    const up = XYtoI(p.x - 1, p.y);
+    const down = XYtoI(p.x + 1, p.y);
+    const left = XYtoI(p.x, p.y - 1);
+    const right = XYtoI(p.x, p.y + 1);
+    const neighbours = [];
+
+    levelUp = grid[level - 1] || emptyLevel;
+    levelDown = grid[level + 1] || emptyLevel;
+    if (current[up] === undefined) {
+        // Add middle second row element from level above.
+        neighbours.push(levelUp[XYtoI(1, 2)]);
+    } else if (current[up] === '?') {
+        // Add all last row element from level below.
+        neighbours.push(levelDown[XYtoI(4, 0)]);
+        neighbours.push(levelDown[XYtoI(4, 1)]);
+        neighbours.push(levelDown[XYtoI(4, 2)]);
+        neighbours.push(levelDown[XYtoI(4, 3)]);
+        neighbours.push(levelDown[XYtoI(4, 4)]);
+    } else {
+        neighbours.push(current[up]);
+    }
+    
+    if (current[down] === undefined) {
+        // Add middle fourth row element from level above.
+        neighbours.push(levelUp[XYtoI(3, 2)]);
+    } else if (current[down] === '?') {
+        // Add all first row element from level below.
+        neighbours.push(levelDown[XYtoI(0, 0)]);
+        neighbours.push(levelDown[XYtoI(0, 1)]);
+        neighbours.push(levelDown[XYtoI(0, 2)]);
+        neighbours.push(levelDown[XYtoI(0, 3)]);
+        neighbours.push(levelDown[XYtoI(0, 4)]);
+    } else {
+        neighbours.push(current[down]);
+    }
+
+    if (current[left] === undefined) {
+        // Add middle second column element from level above.
+        neighbours.push(levelUp[XYtoI(2, 1)]);
+    } else if (current[left] === '?') {
+        // Add all last column element from level below.
+        neighbours.push(levelDown[XYtoI(0, 4)]);
+        neighbours.push(levelDown[XYtoI(1, 4)]);
+        neighbours.push(levelDown[XYtoI(2, 4)]);
+        neighbours.push(levelDown[XYtoI(3, 4)]);
+        neighbours.push(levelDown[XYtoI(4, 4)]);
+    } else {
+        neighbours.push(current[left]);
+    }
+
+    if (current[right] === undefined) {
+        // Add middle fourth column element from level above.
+        neighbours.push(levelUp[XYtoI(2, 3)]);
+    } else if (current[right] === '?') {
+        // Add all first column element from level below.
+        neighbours.push(levelDown[XYtoI(0, 0)]);
+        neighbours.push(levelDown[XYtoI(1, 0)]);
+        neighbours.push(levelDown[XYtoI(2, 0)]);
+        neighbours.push(levelDown[XYtoI(3, 0)]);
+        neighbours.push(levelDown[XYtoI(4, 0)]);
+    } else {
+        neighbours.push(current[right]);
+    }
+
+    return neighbours;
+}
+
+const requireLevelUp = (grid, level) => {
+    const current = grid[level];
+    for (let i = 0; i < current.length; i++) {
+        const p = iToXY(i);
+        if (current[i] === '#') {
+            if (p.x === 0 || p.x === 4 || p.y === 0 || p.y === 4) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+const requireLevelDown = (grid, level) => {
+    const current = grid[level];
+    const p = { x: 2, y: 2 };
+    const up = { x: p.x - 1, y: p.y };
+    const down = { x: p.x + 1, y: p.y };
+    const left = { x: p.x, y: p.y - 1 };
+    const right = { x: p.x, y: p.y + 1 };
+    const neighbours = [up, down, left, right];
+    for (const neighbour of neighbours) {
+        if (current[XYtoI(neighbour.x, neighbour.y)] === '#') {
+            return true;
+        }
+    }
+    return false;
+}
+
+const getNextRecursive = (grid) => {
+    const nextGrid = {};
+    const levels = Object.keys(grid).map(item => Number(item)).sort((a, b) => a - b);
+    if (requireLevelUp(grid, levels[0])) {
+        grid[levels[0] - 1] = emptyLevel;
+    }
+    if (requireLevelDown(grid, levels.slice(-1)[0])) {
+        grid[levels.slice(-1)[0] + 1] = emptyLevel;
+    }
+    Object.keys(grid).map(item => Number(item)).forEach(level => {
+        const next = [];
+        const current = grid[level];
+        for (let i = 0; i < current.length; i++) {
+            const neighbours = getNeighbours(grid, level, i);
+            // console.log(neighbours, current[i], i);
+            let bugCount = 0;
+            for (const neighbour of neighbours) {
+                if (neighbour === '#') {
+                    bugCount++;
+                }
+            }
+            if (current[i] === '#' && bugCount !== 1) {
+                next[i] = '.';
+            } else if (current[i] === '.' && (bugCount === 1 || bugCount === 2)) {
+                next[i] = '#';
+            } else {
+                next[i] = current[i];
+            }
+        }
+        nextGrid[level] = next.join('');
+    });
+    return nextGrid;
+}
+
 process.stdin.on('data', function (chunk) {
     const input = chunk.toString().split('\n').join('');
     // Write your code here
@@ -60,4 +195,32 @@ process.stdin.on('data', function (chunk) {
         }
     }
     console.log(biodirversity);
+    const part2Input = input.split('');
+    part2Input[12] = '?'
+    let grid = {
+        '0': part2Input.join('')
+    };
+
+    // grid = {
+    //     '0': '##.#..#....#?.#.#.#..#..#',
+    //     '1': '######.#.##.?.##...##...#',
+    //     '2': '#...##...##.?.##...##...#',
+    //     '3': '............?............',
+    //     '-1': '.#.#.##..##.?#..##.......',
+    //     '-2': '.......#....?............'
+    // };
+    // console.log(getNextRecursive(grid));
+    for (let i = 0; i < 200; i++) {
+        grid = getNextRecursive(grid);
+    }
+    bugCount = 0;
+    Object.keys(grid).forEach(level => {
+        const current = grid[level];
+        for (let i = 0; i < current.length; i++) {
+            if (current[i] === '#') {
+                bugCount++;
+            }
+        }
+    });
+    console.log(bugCount);
 });
