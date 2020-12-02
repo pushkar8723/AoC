f913e4947f6dfd79514118e9cd8f70cc7dfd750e
Pushkar Anand <pushkar.anand@flipkart.com>
2020-12-02 11:56:39 +0530

    Day 2 solution and template update.

--- scripts/templates/javascript.js
+++ scripts/templates/javascript.js
@@ -2,7 +2,7 @@ process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', function (chunk) {
-    const lines = chunk.toString().split('\n');
+    const lines = chunk.toString().split('\n').filter(item => item);
     // Write your code here
     
 });
